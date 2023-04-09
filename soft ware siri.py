205d76b2938c6f52fc34347e786bb08e65a94c7b
rami <124908204+FaZehulk@users.noreply.github.com>
2023-04-09 09:56:29 +0300

    Add files via upload
    that`s the write one help me with the code

--- soft ware siri.py
+++ soft ware siri.py
@@ -7,11 +7,10 @@ def start():
         print ("let me show you how this works if you tried this just press enter on your key_board to skip ")
         var1=input ("write hi siri to wake sir. up ")
         var2=input ("write sir open ...... , it shood be fortnite or r/v:-  ")
-        url=https//www.crazygames.com/game/buildnow-gg
+        `url=https://www.crazygames.com/game/buildnow-
         if var2 == "fortnite" :
-                print(url) 
+                print("Click me") 
         `url=https://www.reddit.com/
         elif var2 == "r/v" :
                  print("Click me")
-#https//web.whatsapp.com/
 start()
