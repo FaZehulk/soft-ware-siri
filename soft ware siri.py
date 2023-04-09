84b161736d1c578570e3ecde268b429634ccdc94
rami <124908204+FaZehulk@users.noreply.github.com>
2023-04-09 09:54:15 +0300

    Add files via upload

---
+++ soft ware siri.py
@@ -0,0 +1,17 @@
+#____________________soft ware siri________________________
+# by rami           __________________
+'''soft ware siri is a siri but on a laptop or ps'''
+
+def start():
+
+        print ("let me show you how this works if you tried this just press enter on your key_board to skip ")
+        var1=input ("write hi siri to wake sir. up ")
+        var2=input ("write sir open ...... , it shood be fortnite or r/v:-  ")
+        url=https//www.crazygames.com/game/buildnow-gg
+        if var2 == "fortnite" :
+                print(url) 
+        `url=https://www.reddit.com/
+        elif var2 == "r/v" :
+                 print("Click me")
+#https//web.whatsapp.com/
+start()
